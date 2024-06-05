1afe370fbafcf46d58c52fcf81c6aded057f2df0
Attila <hunkrekker@gmail.com>
2024-06-05 08:51:38 +0200

    Added IVoxelize

---
+++ Models/FlatVoxelArray.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class FlatVoxelArray : IVoxelCollection
+    {
+        public VoxelData this[Vector3Int pos] { get => voxels[pos.X+pos.Y*size.Y+pos.Z*Size.Y*Size.Z]; set => voxels[pos.X + pos.Y * size.Y + pos.Z * Size.Y * Size.Z] = value }
+        public VoxelData this[int x, int y, int z] { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public Vector3Int Size { get => size; set => size = value; }
+
+        private VoxelData[] voxels;
+        private Vector3Int[] voxelsPosition;
+        private Vector3Int size;
+    }
+}
---
+++ Models/IVoxelCollection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public interface IVoxelCollection
+    {
+        public VoxelData this[Vector3Int pos] { get;set; }
+        public VoxelData this[int x,int y,int z] { get;set; }
+        public Vector3Int Size { get; set; }
+    }
+}

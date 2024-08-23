e77550e56ffb994dbca52c1d0804ded6f4b5301c
Attila <hunkrekker@gmail.com>
2024-08-23 09:49:25 +0200

    Aded GridInfill

--- CrystaliteSlicer/InfillGeneration/GridInfill.cs
+++ CrystaliteSlicer/InfillGeneration/GridInfill.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,21 @@ using System.Threading.Tasks;
 
 namespace CrystaliteSlicer.InfillGeneration
 {
-    public class GridInfill
+    public class GridInfill : IInfill
     {
+        public int gridSizeX;
+        public int gridSizeY;
+
+        public GridInfill(int gridSizeX, int gridSizeY)
+        {
+            this.gridSizeX = gridSizeX;
+            this.gridSizeY = gridSizeY;
+        }
+
+        public bool IsFill(float distanceToWall, float distanceToCeiling, int x, int y, int z)
+        {
+            return x % Math.Max((int)((1 - Settings.InfillDensity) * gridSizeX), 1) == 0 && y % Math.Max((int)((1 - Settings.InfillDensity) * gridSizeY), 1) == 0;
+        }
+
     }
 }
